b1fc24cba13692dd0f7be8e355bd00f059f17ca2
Jacob9691 <48980223+Jacob9691@users.noreply.github.com>
2024-04-09 08:42:14 +0200

    solved EmployeeV10

---
+++ EmployeesV10/Employee.cs
@@ -0,0 +1,19 @@
+﻿
+namespace EmployeesV10
+{
+    public class Employee
+    {
+        #region Properties
+        public string Name { get; set; }
+        public int HoursPerWeek { get; set; }
+        #endregion
+
+        #region Constructor
+        public Employee(string name, int hoursPerWeek)
+        {
+            Name = name;
+            HoursPerWeek = hoursPerWeek;
+        }
+        #endregion
+    }
+}
---
+++ EmployeesV10/ITSupporter.cs
@@ -0,0 +1,26 @@
+﻿
+using EmployeesV10;
+
+public class ITSupporter : Employee
+{
+    #region Properties
+    public string PrimaryWorkArea { get; set; }
+
+    public string AllInformation
+    {
+        get
+        {
+            return $"IT-Supporter {Name} works {HoursPerWeek} hours/week, mostly with {PrimaryWorkArea}";
+        }
+    }
+    #endregion
+
+    #region Constructor
+    public ITSupporter(string name, int hoursPerWeek, string primaryWorkArea): base(name, hoursPerWeek)
+    {
+        Name = name;
+        HoursPerWeek = hoursPerWeek;
+        PrimaryWorkArea = primaryWorkArea;
+    }
+    #endregion
+}
