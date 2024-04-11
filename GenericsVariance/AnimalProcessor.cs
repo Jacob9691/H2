a81ae53142ec9ea4dec1060721c8e8eff74b6dce
Jacob Woetmann Kastberg <jacob.w.kastberg@hotmail.com>
2024-04-11 14:59:36 +0200

    added in and out

---
+++ GenericsVariance/AnimalProcessor.cs
@@ -0,0 +1,49 @@
+﻿
+public class AnimalProcessor
+{
+    public void ProcessAnimals(ICollectionGet<Animal> animals)
+    {
+        for (int index = 0; index < animals.Count; index++)
+        {
+            Console.WriteLine(animals.Get(index).Sound());
+            // animals.GetAnimal(index).FlapWings(); // Why does this NOT work...? there is no GetAnimals method
+        }
+    }
+
+    public void ProcessBirds(ICollectionGet<Bird> birds)
+    {
+        for (int index = 0; index < birds.Count; index++)
+        {
+            Console.WriteLine(birds.Get(index).Sound());
+            birds.Get(index).FlapWings(); // Why does this work..? It uses the interface to get the bird from the index
+        }
+    }
+
+    public void InsertAnimals(ICollectionSet<Animal> animals)
+    {
+        for (int index = 0; index < 5; index++)
+        {
+            Bird b = new Bird("Tweety");
+            b.FlapWings();
+            animals.Set(b); // Why does this work..? the same with using the interface
+
+            Cat c = new Cat("Spot");
+            c.Purr();
+            animals.Set(c); // Why does this work..? the same with using the interface
+        }
+    }
+
+    public void InsertBirds(ICollectionSet<Bird> birds)
+    {
+        for (int index = 0; index < 5; index++)
+        {
+            Bird b = new Bird("Tweety");
+            b.FlapWings();
+            birds.Set(b);
+
+            Cat c = new Cat("Spot");
+            c.Purr();
+            // birds.SetAnimal(c); // Why does this NOT work...? there is no SetAnimal method
+        }
+    }
+}
---
+++ GenericsVariance/Program.cs
@@ -0,0 +1,38 @@
+﻿
+// No problem, since Collection implements 
+// ICollectionGet and ICollectionSet
+Collection<Bird> birds = new Collection<Bird>();
+ICollectionGet<Bird> birdsGet = birds;
+ICollectionSet<Bird> birdsSet = birds;
+
+// No problem, since Collection implements 
+// ICollectionGet and ICollectionSet
+Collection<Animal> animals = new Collection<Animal>();
+ICollectionGet<Animal> animalsGet = animals;
+ICollectionSet<Animal> animalsSet = animals;
+
+
+AnimalProcessor processor = new AnimalProcessor();
+
+// How many of these work...?
+//
+processor.ProcessAnimals(birdsGet);   // Case A
+processor.ProcessAnimals(animalsGet); // Case B
+
+processor.ProcessBirds(birdsGet);     // Case C
+//processor.ProcessBirds(animalsGet);   // Case D
+
+//processor.InsertAnimals(birdsSet);    // Case E
+processor.InsertAnimals(animalsSet);  // Case F
+
+processor.InsertBirds(birdsSet);      // Case G
+processor.InsertBirds(animalsSet);    // Case H
+
+/*
+ Task notes
+    7: the out makes the interface co-variant, so now the bird class will also work as parameter for a animal method.
+        Since bird inherit from the animal, but this won't work the other way around.
+        So animal won't work as a parameter for a bird method.
+    9: the in makes the opposite of co-variant, now making it contra-variant.
+        So now animals can be used for birds method.
+ */
