bc272824ae2b61b1f60562b7cadba621e77bead2
Jacob9691 <48980223+Jacob9691@users.noreply.github.com>
2024-04-15 10:21:54 +0200

    NumericalPi done

---
+++ NumericalPi/PiCalc.cs
@@ -0,0 +1,52 @@
+﻿
+public class PiCalc
+{
+    /// <summary>
+    /// Executes the calculation of an 
+    /// approximate value of pi.
+    /// </summary>
+    /// <param name="iterations">Number of iterations to perform</param>
+    /// <returns>Approximate value of pi</returns>
+    public double Calculate(int iterations)
+    {
+        //int insideUnitCircle = Iterate(iterations);
+        //return insideUnitCircle * 4.0 / iterations;
+
+        Task<int> insideUnitCircle1 = Task<int>.Run(() => Iterate(iterations / 4));
+        Task<int> insideUnitCircle2 = Task<int>.Run(() => Iterate(iterations / 4));
+        Task<int> insideUnitCircle3 = Task<int>.Run(() => Iterate(iterations / 4));
+        Task<int> insideUnitCircle4 = Task<int>.Run(() => Iterate(iterations / 4));
+
+        Task<int>.WaitAll(insideUnitCircle1, insideUnitCircle2, insideUnitCircle3, insideUnitCircle4);
+        return((
+            insideUnitCircle1.Result + 
+            insideUnitCircle2.Result +  
+            insideUnitCircle3.Result + 
+            insideUnitCircle4.Result) * 4.0 / iterations
+            );
+    }
+
+    /// <summary>
+    /// Perform a number of "dart-throwing" simulations.
+    /// </summary>
+    /// <param name="iterations">Number of dart-throws to perform</param>
+    /// <returns>Number of throws within the unit circle</returns>
+    public int Iterate(int iterations)
+    {
+        Random _generator = new Random(Guid.NewGuid().GetHashCode());
+        int insideUnitCircle = 0;
+
+        for (int i = 0; i < iterations; i++)
+        {
+            double x = _generator.NextDouble();
+            double y = _generator.NextDouble();
+
+            if (x * x + y * y < 1.0)
+            {
+                insideUnitCircle++;
+            }
+        }
+
+        return insideUnitCircle;
+    }
+}
---
+++ NumericalPi/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Diagnostics;
+
+Stopwatch watch = new Stopwatch();
+PiCalc calculator = new PiCalc();
+
+Console.WriteLine("Started");
+watch.Start();
+double numPi = calculator.Calculate(100000000);
+watch.Stop();
+Console.WriteLine("Done");
+
+Console.WriteLine($"Numeric PI = {numPi:0.000000}");
+Console.WriteLine($"Real PI    = {Math.PI:0.000000}");
+Console.WriteLine($"Took {watch.ElapsedMilliseconds} milliSecs");
+Console.WriteLine();
+
+/*
+ Task notes:
+    1: To utilise my 3 new friends, I will give them each a task to do the throws. But I'll divide the trows among us.
+        So now it will speed up, since only one task need to do 25.000.000 throws now, instead of 100.000.000.
+    3: The old version took 3040 milliSecs, while my new version took 1147 milliSecs. So dividing the code to 4 tasks,
+        helps a lot with the speed.
+    4: I have an Intel Core i7 8th gen, so it has 6 cores. And because of this, 
+        my CPU has no problem calculating 4 tasks. That is why the perfomance got improved a lot,
+        by dividing it into tasks.
+ */
