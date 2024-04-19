659ba6be9dc424416ced50c14ba421306c7a748b
Jacob9691 <48980223+Jacob9691@users.noreply.github.com>
2024-04-19 12:31:04 +0200

    Pro 5.1 setup

---
+++ Threads/Threads/HalloFromThread.cs
@@ -0,0 +1,20 @@
+﻿
+public class HalloFromThread
+{
+    private readonly int _id;
+    public HalloFromThread(int id)
+    {
+        _id = id;
+    }
+
+    public void ThreadHallos()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"Hallo #{i} from thread {_id}");
+        }
+
+        Console.WriteLine($"Thread {_id} terminates");
+    }
+
+}
---
+++ Threads/Threads/SharingIsCaring.cs
@@ -0,0 +1,35 @@
+﻿
+public class SharingIsCaring
+{
+    private int share;
+    private readonly object _lock = new ();
+
+    public SharingIsCaring()
+    {
+        share = 0;
+    }
+
+    public void Add()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            lock (_lock) 
+            { 
+                share++;
+                Thread.Sleep(500);
+            }
+        }
+    }
+
+    public void Read()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            lock (_lock) 
+            { 
+                Console.WriteLine(share);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
